65fad046b6d689c00d97b5485782be79693bea63
0xcyborg <104452787+0xcyborg@users.noreply.github.com>
2023-12-23 17:41:53 +0100

    Removing *.

--- Tool.py
+++ Tool.py
@@ -1,23 +1,23 @@
 # 2022-04-26
 
-from urllib3 import PoolManager,disable_warnings
+from urllib3 import PoolManager, disable_warnings
 from json import loads
 
 disable_warnings()
 Http = PoolManager()
 Domain = input("Enter Domain -> ").strip()
-Request = Http.request("GET","https://crt.sh?q={}&output=json".format(Domain))
+Request = Http.request("GET", "https://crt.sh?q={}&output=json".format(Domain))
 Response = loads(Request.data)
 
 List = []
 for Object in Response:
   Domains = Object["name_value"].split("\n")
   for Domain in Domains:
-    Domain = Domain.lower()
+    Domain = Domain.lower().replace("*.", "")
     if(Domain not in List and "@" not in Domain):
-      with open("Result.txt","a",encoding="utf-8") as File:
+      with open("Result.txt", "a", encoding="utf-8") as File:
         File.write(Domain + "\n")
       print(Domain)
       List.append(Domain)
 else:
-  print("Done")
+  print("~ Done")
